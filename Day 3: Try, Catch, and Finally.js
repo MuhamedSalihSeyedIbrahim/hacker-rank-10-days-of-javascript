2d1dbff7e3a3bb7be06d52a5d48d487c4eaa35c2
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-05-20 11:49:34 +0530

    Create Day 3: Try, Catch, and Finally.js

---
+++ Day 3: Try, Catch, and Finally.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function reverseString(s) {
+   try {
+        var splitString = s.split("");
+        var reverseArray = splitString.reverse();
+        var joinArray = reverseArray.join("");
+        console.log(joinArray);
+    }
+    catch (error)
+        {
+            // Print s on a new line. If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+            console.log(error.message);
+            console.log(s);}
+
+}
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
