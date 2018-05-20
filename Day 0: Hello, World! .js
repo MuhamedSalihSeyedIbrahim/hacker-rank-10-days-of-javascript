dd551494fdef43f2eea9a9326dd2173d0867b4c1
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-05-20 11:36:49 +0530

    Create Day 0: Hello, World! .js

---
+++ Day 0: Hello, World! .js
@@ -0,0 +1,26 @@
+
+'use strict';
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString = '';
+let currentLine = 0;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+function readLine() {
+    return inputString[currentLine++];
+}
+function greeting(x) {
+    console.log('Hello, World!\n'+x);
+}
+function main() { 
+  const parameterVariable = readLine(); 
+  greeting(parameterVariable); 
+}
