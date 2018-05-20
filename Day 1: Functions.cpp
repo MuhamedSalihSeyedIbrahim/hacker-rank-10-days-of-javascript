780032fd9594e707dbbdd1c05805e8d52e67ce8c
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-05-20 11:40:55 +0530

    Create Day 1: Functions.cpp

---
+++ Day 1: Functions.cpp
@@ -0,0 +1,34 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function factorial(a){
+    var x=1;
+    for(var i=1;i<=a;i++)x*=i;
+    return x;
+}
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
