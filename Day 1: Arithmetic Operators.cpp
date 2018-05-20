ec3a33406b1197a38bd1b3c0a21ec4e9228d4d30
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-05-20 11:39:20 +0530

    Create Day 1: Arithmetic Operators.cpp

---
+++ Day 1: Arithmetic Operators.cpp
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getArea(l, w) {
+    return (parseFloat(w)*parseFloat(l));
+}
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    
+    return 2*(parseFloat(length)+parseFloat(width));
+}
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
