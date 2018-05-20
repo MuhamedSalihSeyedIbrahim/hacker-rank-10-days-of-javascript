fc4d7ed5e0c69cbe44b9aa7a9dbb7f6aa891ddfb
MuhamedSalih <30931525+muhamedsalih21@users.noreply.github.com>
2018-05-20 11:38:14 +0530

    Create Day 0: Data Types.js

---
+++ Day 0: Data Types.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    
+    //console.log(firstInteger+" "+firstDecimal+" "+firstString);
+     console.log((firstInteger+parseInt(secondInteger))+"\n"+(parseFloat(secondDecimal)+firstDecimal)+"\n"+(firstString+secondString));
+}
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
